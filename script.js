82bf14fc1a52ef827cf12712f158fd0f3bf5e1a1
izotych <alex.izotoff2012@yandex.ru>
2021-01-03 19:37:18 +0300

    7 kata

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-let x = 01000001;
-y = parseInt(x, 2).toString (10);
-console.log(y);
-//patau
+let x = 2017;
+function arrayOfNum (num) {
+    x = x + '';
+    console.log(x);
+    let arr = [];
+    for (let i = 1, y; i <= x.length; i++ ){
+        y = x.substring(0, i);
+        console.log(y);
+        arr.push(y);
+    }
+    return arr;
+}
+console.log(arrayOfNum(x));
